c825ef1cabe8d6f7087b6101be942599b4c8c5b1
JunioResende <junioresende23@gmail.com>
2023-03-16 16:54:09 -0300

    ex25

---
+++ ex24.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+if num1 > num2:
+    print(f"O primeiro numero {num1} e maior!")
+else:
+    print(f"O segundo numero {num2} e maior!")
