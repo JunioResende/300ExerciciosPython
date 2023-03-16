87db219e7c2efdc5c9ab1c4d63d50b0daab5c06f
JunioResende <junioresende23@gmail.com>
2023-03-16 11:51:34 -0300

    ex12

---
+++ ex12.py
@@ -0,0 +1,11 @@
+"""
+    Some os valores das variaveis num1 e num2
+    Atribuindo a soma a uma nova variavel
+    Exiba em tela o conteudo dessa variavel
+"""
+
+num1 = 52
+num2 = 106
+soma = num1 + num2
+
+print(f"O valor da varivel soma e {soma}")
