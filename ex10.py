aff6a86c0381f14318811bb1cd94f9128e61716b
JunioResende <junioresende23@gmail.com>
2023-03-16 11:40:41 -0300

    ex10

---
+++ ex10.py
@@ -0,0 +1,10 @@
+"""
+    Mostre o tamanho da lista nomes (numero de elementos da lista nomes)
+    Mostre separadamente apenas o terceiro elemento dessa lista
+"""
+
+nomes = ["Junio", "Isa", "Paula", "Ana", "Clara"]  # Criacao da lista
+
+# Mostra em tela a quantidade de items na lista e o terceiro elemento dessa lista. Toda lista comeca do indice 0
+print(
+    f"A lista nomes tem {len(nomes)} elementos e o terceiro elemento dessa lista e o nome {nomes[3]} ")
