23d72ed35da7a80dac756a4380d2b4d5189416f8
JunioResende <junioresende23@gmail.com>
2023-03-16 15:48:05 -0300

    ex23

---
+++ ex23.py
@@ -0,0 +1,6 @@
+num = 100
+
+lista_num = [10, 100, 1000, 10000]
+
+# O operador in e muito util para verificar se um valor ou dadp consta dentro de uma varivavel ou objeto.
+print(f"O numero 100 consta na lista_num? {num in lista_num}")
