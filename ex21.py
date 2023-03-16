ad85b723884bce67fa563e66ea6ff1907cc0a4b0
JunioResende <junioresende23@gmail.com>
2023-03-16 15:09:44 -0300

    ex21

---
+++ ex21.py
@@ -0,0 +1,6 @@
+# Dadas duas variaveis num1 e num2 com valores 100 e 89, respectivament, verifique se o valor de num1 e num2 sao menores ou iguais 100
+
+num1 = 100
+num2 = 89
+
+print(f"num1 e num2 sao menores ou iguais a 100? {num1 and num2 <= 100}")
