303bdc7ceea4fa7ec5600f42d0f2650138823764
JunioResende <junioresende23@gmail.com>
2023-03-16 15:13:11 -0300

    ex22

---
+++ ex22.py
@@ -0,0 +1,6 @@
+# Dadas duas variaveis num1 e num2 com valores 100 e 89, respectivament, verifique se o valor de num1 ou num2 sao maiores ou iguais 100
+
+num1 = 100
+num2 = 89
+
+print(f"num1 e num2 sao maiores ou iguais a 100? {num1 or num2 >= 100}")
