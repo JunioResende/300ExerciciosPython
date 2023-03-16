a41a1e1f893874d4681a45616f55945400edc500
JunioResende <junioresende23@gmail.com>
2023-03-16 10:53:10 -0300

    ex06

---
+++ ex06.py
@@ -0,0 +1,6 @@
+# Exiba em tela o valor e o tipo de dado da variavel num1: Sendo num1 = 1987
+
+num1 = 1987  # Variavel
+
+print(
+    f"O numero armazenado na variavel num1 e {num1} e o seu tipo e {type(num1)}")  # Aqui imprimimos o valor da variavel e seu tipo
