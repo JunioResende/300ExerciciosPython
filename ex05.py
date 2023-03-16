8ae801ca1fbeff7ab13cfe11d30c3eb97eb42daa
JunioResende <junioresende23@gmail.com>
2023-03-16 10:27:15 -0300

    ex04

---
+++ ex05.py
@@ -0,0 +1,10 @@
+# Crie uma variavel nome e atribua para a mesma um nome digitado pelo usuario
+
+# Para isso iremos usar a funcao input() que coleta os dados digitados pelo usuario
+nome = input("Digite seu nome: ")
+# Ao escrever o nome no terminal apertando enter o texto digitado estara atribuido a variavel nome
+
+# Indo alem
+
+# Aqui coletamos o nome digitado no terminal e mostramos em tela. Alem disso tambem mostramos o tipo da variavel
+print(f"Seu nome e... {nome} e o tipo da variavel e... {type(nome)}")
