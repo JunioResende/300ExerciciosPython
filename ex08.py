50fab03de741f8758515ad479b0fae6f658a3da2
JunioResende <junioresende23@gmail.com>
2023-03-16 11:26:44 -0300

    fix ex08

--- ex08.py
+++ ex08.py
@@ -2,7 +2,10 @@
 exibindo em tela tanto o numero em si quanto seu tipo de dado
 """
 
+# Variavel que ira captar um numero inteiro
 num = int(input("Digite um numero inteiro: "))
-num = float(num)
+num_conv = float(num)  # Variavel que ira fazer a conversao
 
-print(f"O numero digitado e {num} e seu tipo e {type(num)}")
+# Mostra em tela o numero digitado pelo usuario e seu tipo de dado
+print(
+    f"O numero digitado e {num} e Seu tipo de dado e {type(num)},\n apos a conversao para float temos {num_conv:.2f} e seu tipo e {type(num_conv)}")
