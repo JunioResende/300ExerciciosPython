2724fa7923d00eee8c50d9d0fe0669be3e218803
JunioResende <junioresende23@gmail.com>
2023-03-16 10:15:23 -0300

    ex04

---
+++ ex04.py
@@ -0,0 +1,5 @@
+# Escreva um programa que mostre em tela a mensagem "Ola Mundo!"
+
+mensagem = "Ola Mundo!"  # Variavel que contem a mensagem
+
+print(mensagem)  # Mostra em tela o valor da variavel.
