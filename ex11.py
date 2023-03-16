474ef8baceacdf824ea7d2e540c6145ee2179dc8
JunioResende <junioresende23@gmail.com>
2023-03-16 11:47:06 -0300

    ex11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+"""
+    Some os valores das variaveis num1 e num2
+    Por fim exiba em tela o resultado da soma.
+"""
+
+num1 = 52
+num2 = 106
+
+# Soma as duas variaveis e mostra em tela
+print(f"A soma de num1 e num2 e igual a {num1 + num2}")
