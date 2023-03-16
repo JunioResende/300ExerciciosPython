cc4c9d009f56fa15dd9e2f51e1aced88e65d9075
JunioResende <junioresende23@gmail.com>
2023-03-16 15:03:09 -0300

    ex18

---
+++ ex18.py
@@ -0,0 +1,6 @@
+# Dadas duas variaveis num1 e num2 com valores 100 e 89, respectivament, verifique se o valor de num1 e igual a num2
+
+num1 = 100
+num2 = 89
+
+print(f"num1 e igual num2? {num1 == num2}")
