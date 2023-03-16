22317e436e2790899c890dd1b453ada76194edc2
JunioResende <junioresende23@gmail.com>
2023-03-16 10:00:47 -0300

    ex01

---
+++ ex01.py
@@ -0,0 +1,11 @@
+# Crie Tres variaveis com tres tipos de dados diferentes, respeitando sua sintaxe.
+var1_str = "Junio"  # Variavel do tipo string(texto) str
+var2_int = 32  # Variavel do tipo integer (numero inteiro) int
+var3_float = 131.82  # Variavel do tipo float (flutuante, decimal, real) float
+
+# Mostra em tela o valor da variavel e o tipo dela
+print(var1_str, type(var1_str))
+# Mostra em tela o valor da variavel e o tipo dela
+print(var2_int, type(var2_int))
+# Mostra em tela o valor da variavel e o tipo dela
+print(var3_float, type(var3_float))
