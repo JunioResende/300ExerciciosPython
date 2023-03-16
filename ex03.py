b75fc09e66e4132e62a78ab8e632543f06248fad
JunioResende <junioresende23@gmail.com>
2023-03-16 10:12:51 -0300

    ex03

---
+++ ex03.py
@@ -0,0 +1,5 @@
+'''
+    Os comentarios de mais de uma linha sao uteis para explicar
+    de uma forma mais elaborada o codigo em questao.
+    Sao delimitados por por 3 aspas siples ou 3 aspas duplas
+'''
