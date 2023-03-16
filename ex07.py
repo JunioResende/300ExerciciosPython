1fb858058e65e8d13ee30687f36d5e1f78c9730d
JunioResende <junioresende23@gmail.com>
2023-03-16 10:59:17 -0300

    ex07

---
+++ ex07.py
@@ -0,0 +1,8 @@
+"""
+Peca para que o usuario digite um numero,
+em seguida exiba em tela o numero digitado.
+"""
+
+num = input("Digite um numero")  # Variavel que ira captar o numero digitado
+
+print(f"O numero digitado e {num}")  # impressao do valor digitado
