3a7503b285aab3da64b23d82862f6053ad26d4ec
JunioResende <junioresende23@gmail.com>
2023-03-16 12:09:54 -0300

    ex14

---
+++ ex14.py
@@ -0,0 +1,9 @@
+# Realize as operacoes de multiplicacao e de divisao entr os valores das variaveis num1 e num2.
+
+num1 = 52
+num2 = 106
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(
+    f"A multiplicacao de num1 e num2 tem o resultado: {multiplicacao}\nA divisao de num1 e num2 tem o resultado {divisao: .2f}")
