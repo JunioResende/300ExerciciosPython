a9abaebef6b4cea4789e0b37091b004ab141d174
JunioResende <junioresende23@gmail.com>
2023-03-16 12:51:16 -0300

    ex15

---
+++ ex15.py
@@ -0,0 +1,7 @@
+# Eleve o valor de num1 a oitava potencia, sendo num1 = 51.
+
+num1 = 51
+potencia = 8
+potenciacao = num1 ** potencia
+
+print(f"{num1} eleado a oitava potencia (x{potencia} ou x⁸) resulta em: {potenciacao}")
