a0425e6dc0ae493238ad514746329a10de576c73
JunioResende <junioresende23@gmail.com>
2023-03-17 08:21:32 -0300

    ex30

---
+++ ex30.py
@@ -0,0 +1,3 @@
+for i in range(0, 20):  # Lembrando que sempre onde tem intervalos podemos utilizar o metodo range
+    if i % 2 == 0:  # Para contar apenas os numeros pares de uma contagem de 0 a 20 dividimos o resto da divisao por 2
+        print(i)
